c29b9089330969668948df1c10e349801b05806e
Amit Chavhan <amitc005@gmail.com>
2021-01-20 12:20:09 -0500

    Removed the secrete information from send email.

--- send_email.py
+++ send_email.py
@@ -1,10 +1,12 @@
 from smtplib import SMTP
 from datetime import datetime
 
+email = "EMAIL" # replace this 
+password = "PASSWORD" # replace this
 smtp_client = SMTP("smtp.gmail.com", int(587))
 smtp_client.ehlo()
 smtp_client.starttls()
-smtp_client.login("asiasupport@texadasoftware.com", "Asia+291")
+smtp_client.login(email, password)
 subj = "hello"
 date = datetime.now().strftime("%d/%m/%Y %H:%M")
 
